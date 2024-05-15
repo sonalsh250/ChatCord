6b0f3032b62d2f702c6de7820975d72f3528c054
Sonal <skumari@dovercorp.com>
2024-05-15 18:34:33 +0530

    got front end from github

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require('express'); //regular express server
 
 const app = express();
+
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
 const PORT = 3000 || process.env.PORT;
 //checks if we have an environment variable called port and use that, otherwise use 3000
 app.listen(PORT, ()=> console.log(`Server running on ${PORT}`)); 
